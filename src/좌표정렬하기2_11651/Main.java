c77c05e139fe48ff2935a444ec2ebdddc1bbb742
jigyu12 <74821710+jigyu12@users.noreply.github.com>
2022-03-16 11:01:29 +0900

    Add files via upload

---
+++ src/좌표정렬하기2_11651/Main.java
@@ -0,0 +1,32 @@
+package 좌표정렬하기2_11651;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int[][] xy = new int[n][2];
+        for(int i = 0; i < n; i++){
+            xy[i][1] = input.nextInt();
+            xy[i][0] = input.nextInt();
+        }
+        Arrays.sort(xy, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if(o1[0] == o2[0]) {
+                    return o1[1] - o2[1];
+                }
+                else {
+                    return o1[0] - o2[0];
+                }
+            }
+        });
+        for(int i = 0; i < n; i++){
+            System.out.print(xy[i][1] + " ");
+            System.out.println(xy[i][0]);
+        }
+
+        input.close();
+    }
+}
